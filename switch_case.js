fd355d83f3a9dcfe8d88bd625bf2a2d249ad262f
twinkle <you@example.com>
2019-06-25 19:45:40 +0530

    update switch case file

---
+++ switch_case.js
@@ -0,0 +1,17 @@
+var a=5;
+switch(a){
+    case 0:
+    console.log("0 is here");
+    break;
+    case 1:
+    console.log("1  is here");
+    break;
+    case 2:
+    console.log("2 is here");
+    break;
+    case 3:
+    console.log("3 is here");
+    break;
+    default:
+    console.log("no. is not here");
+}
